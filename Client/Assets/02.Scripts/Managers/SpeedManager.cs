b639e7fe730d4590c47038a351d6086cbe8eb484
ChanheeKim <cgim971@gmail.com>
2022-12-10 00:37:07 +0900

    포탈 VFX

--- Client/Assets/02.Scripts/Managers/SpeedManager.cs
+++ Client/Assets/02.Scripts/Managers/SpeedManager.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class SpeedManager : MonoBehaviour
 {
 
+    #region Public Property
+    public float Speed => _speed;
+    #endregion
+
     private const float _initSpeed = 1f;
     [SerializeField] private float _speed = 1f;
-    public float Speed => _speed;
 
     private void Start()
     {
@@ -15,9 +18,7 @@ public class SpeedManager : MonoBehaviour
     }
 
     // To do : 속도 변경 오브젝트와 닿으면 속도 변경 그 외는 아직 고민
-    public void SetSpeed(float speed = _initSpeed)
-    {
-        _speed = speed;
-    }
+    public void SetSpeed(float speed = _initSpeed) => _speed = speed;
+    public void SetSpeed() => _speed = _initSpeed;
 
 }
